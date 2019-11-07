dfd04a09af72df31718691480869c76b46e7ae19
Jørund Jenssen Berg <jjberg@Jrunds-iMac.local>
2019-11-07 19:16:58 +0100

    Reaching json key with require

--- readWrite/readWrite.js
+++ readWrite/readWrite.js
@@ -1,8 +1,9 @@
 let fs = require("fs");
 let navn = require("./data.json");
 
-console.log(navn);
+console.log(navn.name);
 
 fs.readFile("./data.json", "utf-8", (err, data) => {
-    console.log(data);
+    var data = JSON.parse(data);
+    console.log(data.name);
 })
