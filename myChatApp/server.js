a22b8e18c263cd82c08ccdd0f9ca820f0757244b
Jørund Jenssen Berg <jjberg@Jrunds-iMac.local>
2019-11-09 08:56:33 +0100

    Modifying JavaScript in both index.html and server.js

--- myChatApp/server.js
+++ myChatApp/server.js
@@ -2,6 +2,17 @@ let express = require("express");
 let app = express();
 
 app.use(express.static(__dirname));
+
+let messages = [
+    {name: "Audun", message: "E du der?"},
+    {name: "Jørund", message: "Jepp"},
+]
+
+app.get("/messages", (req, res) => {
+    res.send(messages)
+})
+
+
 let server = app.listen(3000, () => {
     console.log("Serveren lytter til port", server.address().port)
 });
