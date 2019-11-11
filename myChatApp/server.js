3652f8088ccde75e54e1b895f73c3939472579f0
Jørund Jenssen Berg <jjberg@Jrunds-iMac.local>
2019-11-11 06:47:00 +0100

    Starting to use Postman and something called body-parser

--- myChatApp/server.js
+++ myChatApp/server.js
@@ -1,7 +1,9 @@
 let express = require("express");
+let bodyParser = require("body-parser");
 let app = express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
 
 let messages = [
     {name: "Audun", message: "E du der?"},
@@ -12,6 +14,10 @@ app.get("/messages", (req, res) => {
     res.send(messages)
 })
 
+app.post("/messages", (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
+})
 
 let server = app.listen(3000, () => {
     console.log("Serveren lytter til port", server.address().port)
